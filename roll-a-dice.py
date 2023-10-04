aece9b4004711196e6e0936badc6368d851679d7
TuhinaPandit <76855653+TuhinaPandit@users.noreply.github.com>
2023-10-03 21:50:37 -0500

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,27 @@
+import random 
+
+response = "y"
+
+while response == "y": 
+    
+    no = random.randint(1,6)
+    if (no == 1):
+        print(1)
+
+    elif (no == 2):
+        print(2)
+
+    elif (no == 3):
+        print(3)
+
+    elif (no == 4):
+        print(4)   
+
+    elif (no == 5):
+        print(5)
+
+    elif (no == 6):
+        print(6)
+
+    response = input("Do you want to roll the dice again (y for yes, n for no)")
+    
